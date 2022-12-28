7bb615b54539048788c2e1e27543bf6e4c6b2af2
Owen Garrett <owengarrett@Owens-Air.lan>
2022-12-27 20:11:37 -0500

    created questions.js

---
+++ questions.js
@@ -0,0 +1,42 @@
+var questions = [
+  {
+    question: "What does HTML stand for?",
+    option1: "HyperText Markup Language",
+    option2: "wHo Took My Lunch?",
+    option3: "Hello Team Match Language",
+    option4: "Hypertext Matchup Logic",
+    answer: "1",
+  },
+  {
+    question: "What does CSS stand for?",
+    option1: "HyperText Markup Language",
+    option2: "Cascading Simmersheets",
+    option3: "Cascading Stylesheets",
+    option4: "Cybersecurity Systems",
+    answer: "3",
+  },
+  {
+    question: "console.log will __ to the console.",
+    option1: "list",
+    option2: "log",
+    option3: "calculate",
+    option4: "print",
+    answer: "print",
+  },
+  {
+    question: "True/False statements are examples of what?",
+    option1: "Array",
+    option2: "Heading",
+    option3: "Comparison",
+    option4: "Boolean",
+    answer: "Boolean",
+  },
+   {
+    question: "What tag creates an unordered list?",
+    option1: "<H1>",
+    option2: "<ul>",
+    option3: "<ol>",
+    option4: "<header>",
+    answer: "<ul>",
+  }
+];
